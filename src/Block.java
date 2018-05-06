40dc72602f0b7780570de1055ce1d448d479d2e4
Rene <Rene>
2018-05-05 19:11:40 -0700

    Added cube outline

--- src/Block.java
+++ src/Block.java
@@ -1,3 +1,4 @@
+
 /**
  * File :
  * Author :
@@ -8,79 +9,127 @@
 
 import static org.lwjgl.opengl.GL11.*;
 
-class Block
-{
+class Block {
+
     //  Method : Block
     // Purpose :
-    Block () { }
+    Block() {
+    }
 
     //  Method : renderSampleBlock
     // Purpose :
-    void renderSampleBlock ()
-    {
-        glBegin ( GL_QUADS );
+    void renderSampleBlock() {
+        glBegin(GL_QUADS);
 
-        glColor3f ( 1.0f, 0.0f, 0.0f );
+        glColor3f(1.0f, 0.0f, 0.0f);
         // Top Right Of The Quad (Top)
-        glVertex3f ( 1.0f, 1.0f, -1.0f );
+        glVertex3f(1.0f, 1.0f, -1.0f);
         // Top Left Of The Quad (Top)
-        glVertex3f ( -1.0f, 1.0f, -1.0f );
+        glVertex3f(-1.0f, 1.0f, -1.0f);
         // Bottom Left Of The Quad (Top)
-        glVertex3f ( -1.0f, 1.0f, 1.0f );
+        glVertex3f(-1.0f, 1.0f, 1.0f);
         // Bottom Right Of The Quad (Top)
-        glVertex3f ( 1.0f, 1.0f, 1.0f );
+        glVertex3f(1.0f, 1.0f, 1.0f);
 
-        glColor3f ( 0.0f, 1.0f, 0.0f );
+        glColor3f(0.0f, 1.0f, 0.0f);
         // Top Right Of The Quad (Bottom)
-        glVertex3f ( 1.0f, -1.0f, 1.0f );
+        glVertex3f(1.0f, -1.0f, 1.0f);
         // Top Left Of The Quad (Bottom)
-        glVertex3f ( -1.0f, -1.0f, 1.0f );
+        glVertex3f(-1.0f, -1.0f, 1.0f);
         // Bottom Left Of The Quad (Bottom)
-        glVertex3f ( -1.0f, -1.0f, -1.0f );
+        glVertex3f(-1.0f, -1.0f, -1.0f);
         // Bottom Right Of The Quad (Bottom)
-        glVertex3f ( 1.0f, -1.0f, -1.0f );
+        glVertex3f(1.0f, -1.0f, -1.0f);
 
-        glColor3f ( 0.0f, 0.0f, 1.0f );
+        glColor3f(0.0f, 0.0f, 1.0f);
         // Top Right Of The Quad (Front)
-        glVertex3f ( 1.0f, 1.0f, 1.0f );
+        glVertex3f(1.0f, 1.0f, 1.0f);
         // Top Left Of The Quad (Front)
-        glVertex3f ( -1.0f, 1.0f, 1.0f );
+        glVertex3f(-1.0f, 1.0f, 1.0f);
         // Bottom Left Of The Quad (Front)
-        glVertex3f ( -1.0f, -1.0f, 1.0f );
+        glVertex3f(-1.0f, -1.0f, 1.0f);
         // Bottom Right Of The Quad (Front)
-        glVertex3f ( 1.0f, -1.0f, 1.0f );
+        glVertex3f(1.0f, -1.0f, 1.0f);
 
-        glColor3f ( 0.5f, 0.5f, 0.5f );
+        glColor3f(0.5f, 0.5f, 0.5f);
         // Bottom Left Of The Quad (Back)
-        glVertex3f ( 1.0f, -1.0f, -1.0f );
+        glVertex3f(1.0f, -1.0f, -1.0f);
         // Bottom Right Of The Quad (Back)
-        glVertex3f ( -1.0f, -1.0f, -1.0f );
+        glVertex3f(-1.0f, -1.0f, -1.0f);
         // Top Right Of The Quad (Back)
-        glVertex3f ( -1.0f, 1.0f, -1.0f );
+        glVertex3f(-1.0f, 1.0f, -1.0f);
         // Top Left Of The Quad (Back)
-        glVertex3f ( 1.0f, 1.0f, -1.0f );
+        glVertex3f(1.0f, 1.0f, -1.0f);
 
-        glColor3f ( 1.0f, 0.0f, 1.0f );
+        glColor3f(1.0f, 0.0f, 1.0f);
         // Top Right Of The Quad (Left)
-        glVertex3f ( -1.0f, 1.0f, 1.0f );
+        glVertex3f(-1.0f, 1.0f, 1.0f);
         // Top Left Of The Quad (Left)
-        glVertex3f ( -1.0f, 1.0f, -1.0f );
+        glVertex3f(-1.0f, 1.0f, -1.0f);
         // Bottom Left Of The Quad (Left)
-        glVertex3f ( -1.0f, -1.0f, -1.0f );
+        glVertex3f(-1.0f, -1.0f, -1.0f);
         // Bottom Right Of The Quad (Left)
-        glVertex3f ( -1.0f, -1.0f, 1.0f );
+        glVertex3f(-1.0f, -1.0f, 1.0f);
 
-        glColor3f ( 0.0f, 1.0f, 1.0f );
+        glColor3f(0.0f, 1.0f, 1.0f);
         // Top Right Of The Quad (Right)
-        glVertex3f ( 1.0f, 1.0f, -1.0f );
+        glVertex3f(1.0f, 1.0f, -1.0f);
         // Top Left Of The Quad (Right)
-        glVertex3f ( 1.0f, 1.0f, 1.0f );
+        glVertex3f(1.0f, 1.0f, 1.0f);
         // Bottom Left Of The Quad (Right)
-        glVertex3f ( 1.0f, -1.0f, 1.0f );
+        glVertex3f(1.0f, -1.0f, 1.0f);
         // Bottom Right Of The Quad (Right)
-        glVertex3f ( 1.0f, -1.0f, -1.0f );
+        glVertex3f(1.0f, -1.0f, -1.0f);
 
-        glEnd ();
+        glEnd();
+
+        glBegin(GL_LINE_LOOP);
+
+        glColor3f(0.0f, 0.0f, 0.0f);
+        glVertex3f(1.0f, 1.0f, -1.0f);
+        glVertex3f(-1.0f, 1.0f, -1.0f);
+        glVertex3f(-1.0f, 1.0f, 1.0f);
+        glVertex3f(1.0f, 1.0f, 1.0f);
+        glEnd();
+
+        glBegin(GL_LINE_LOOP);
+
+        glVertex3f(1.0f, -1.0f, 1.0f);
+        glVertex3f(-1.0f, -1.0f, 1.0f);
+        glVertex3f(-1.0f, -1.0f, -1.0f);
+        glVertex3f(1.0f, -1.0f, -1.0f);
+        glEnd();
+
+        glBegin(GL_LINE_LOOP);
+
+        glVertex3f(1.0f, 1.0f, 1.0f);
+        glVertex3f(-1.0f, 1.0f, 1.0f);
+        glVertex3f(-1.0f, -1.0f, 1.0f);
+        glVertex3f(1.0f, -1.0f, 1.0f);
+        glEnd();
+
+        glBegin(GL_LINE_LOOP);
+
+        glVertex3f(1.0f, -1.0f, -1.0f);
+        glVertex3f(-1.0f, -1.0f, -1.0f);
+        glVertex3f(-1.0f, 1.0f, -1.0f);
+        glVertex3f(1.0f, 1.0f, -1.0f);
+        glEnd();
+        glBegin(GL_LINE_LOOP);
+
+        glVertex3f(-1.0f, 1.0f, 1.0f);
+        glVertex3f(-1.0f, 1.0f, -1.0f);
+        glVertex3f(-1.0f, -1.0f, -1.0f);
+        glVertex3f(-1.0f, -1.0f, 1.0f);
+        glEnd();
+
+        glBegin(GL_LINE_LOOP);
+
+        glVertex3f(1.0f, 1.0f, -1.0f);
+        glVertex3f(1.0f, 1.0f, 1.0f);
+        glVertex3f(1.0f, -1.0f, 1.0f);
+        glVertex3f(1.0f, -1.0f, -1.0f);
+        glEnd();
     }
 
 }
